35530138210259325f6c539351b2e5ac2bbd581b
Nikos Oikonomou <nikoikonomou92@gmail.com>
2023-01-17 22:24:07 +0200

    core: now has a module definition

---
+++ libs/core/src/core.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from './logger/logger.module';
+import { SecurityModule } from './security/security.module';
+import { ErrorHandlingModule } from './error-handling/error-handling.module';
+import { CookiesModule } from './cookies/cookies.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    LoggerModule /* global */,
+    SecurityModule /* must be imported before other modules as it applies some security-related middleware */,
+    ErrorHandlingModule,
+    ValidationModule,
+    CookiesModule,
+  ],
+})
+export class ValidationModule {}
