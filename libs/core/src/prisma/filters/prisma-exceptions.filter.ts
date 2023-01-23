eae0dd7698809b1be192162db7d01a10a2c5f86a
Nikos Oikonomou <nikoikonomou92@gmail.com>
2023-01-23 23:43:19 +0200

    core: introduced prisma exceptions filter

---
+++ libs/core/src/prisma/filters/prisma-exceptions.filter.ts
@@ -0,0 +1,50 @@
+import { ArgumentsHost, HttpException, Inject } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import { Logger } from 'winston';
+import { LOGGER } from '@app/core/logger/factories/logger.factory';
+import { AppInternalServerErrorException } from '@app/core/error-handling/exceptions/app-internal-server-error.exception';
+import { AppNotFoundException } from '@app/core/error-handling/exceptions/app-not-found.exception';
+import { AppBadRequestException } from '@app/core/error-handling/exceptions/app-bad-request.exception';
+
+/**
+ * A generic prisma-based exceptions filter that translates known prisma exceptions into
+ * application-specific HttpException exceptions. If the exception is not recognized, it
+ * is treated as an internal server error. This filter must be extended with an appropriate
+ * @Catch() to handle prisma errors only.
+ * (e.g. @Catch(Prisma.PrismaClientKnownRequestError, Prisma.NotFoundError).
+ */
+export class PrismaExceptionsFilter extends BaseExceptionFilter {
+  constructor(@Inject(LOGGER) private readonly logger: Logger) {
+    super();
+  }
+
+  catch(exception: any, host: ArgumentsHost) {
+    super.catch(this.exceptionToHttp(exception), host);
+  }
+
+  /**
+   * Converts provided prisma exception to HttpException.
+   * (based on https://www.prisma.io/docs/reference/api-reference/error-reference)
+   * @param {any} exception
+   * @returns {HttpException}
+   */
+  exceptionToHttp(exception: any): HttpException {
+    switch (exception.code) {
+      case 'P2002':
+        return new AppBadRequestException(
+          'Unique Constraint Violation',
+          'unique_constraint',
+        );
+      case 'P2025':
+        return new AppNotFoundException();
+    }
+    this.logger.error('Unexpected prisma error', {
+      type: 'UNEXPECTED_PRISMA_ERROR',
+      error: exception,
+      code: exception.code,
+      meta: exception.meta,
+      clientVersion: exception.clientVersion,
+    });
+    return new AppInternalServerErrorException();
+  }
+}
