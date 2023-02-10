2294dec7899a877cc4aeedf8cb6cbafe40b4c765
Nikos Oikonomou <nikoikonomou92@gmail.com>
2023-02-10 23:35:01 +0200

    search: installed core module and setup main.ts

--- apps/search/src/app.module.ts
+++ apps/search/src/app.module.ts
@@ -1,8 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module, OnApplicationBootstrap } from '@nestjs/common';
+import { CoreModule } from '@app/core/core.module';
+import { LOGGER } from '@app/core/logger/factories/logger.factory';
+import { Logger } from 'winston';
 
 @Module({
-  imports: [],
+  imports: [
+    CoreModule /* must be imported before other modules as it applies some security-related middleware */,
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements OnApplicationBootstrap {
+  constructor(@Inject(LOGGER) private logger: Logger) {}
+
+  onApplicationBootstrap(): any {
+    this.logger.info('Search app bootstrap success!', {
+      type: 'SEARCH_BOOTSTRAP',
+    });
+  }
+}
--- apps/search/src/main.ts
+++ apps/search/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestJSLoggerService } from '@app/core/logger/services/nestjs-logger.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(NestJSLoggerService));
+  const version = 1;
+  const port = 4001;
+  const apiPath = `private/api/v${version}/search`;
+  app.setGlobalPrefix(apiPath);
+  const config = new DocumentBuilder()
+    .setTitle('Search SVC API')
+    .setVersion(`${version}.0`)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(apiPath, app, document);
+  await app.listen(port);
 }
+
 bootstrap();
