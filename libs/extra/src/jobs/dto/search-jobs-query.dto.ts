7d981076e5ccb380de0ab33c6cf7dbc45a5f2cf8
Nikos Oikonomou <nikoikonomou92@gmail.com>
2023-02-11 22:54:38 +0200

    search: jobs: upgraded search-jobs-query
    Now includes:
    - search text
    - employmentType (filter)
    - salaryFrom/salaryTo (filter)

--- libs/extra/src/jobs/dto/search-jobs-query.dto.ts
+++ libs/extra/src/jobs/dto/search-jobs-query.dto.ts
@@ -1,5 +1,9 @@
 import { LimitParam } from '@app/core/pagination/decorators/limit-param.decorator';
 import { PageParam } from '@app/core/pagination/decorators/page-param.decorator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEnum, IsNumber, IsPositive, IsString } from 'class-validator';
+import { EmploymentType } from '@app/jobs/prisma-client';
+import { IsGreaterThan } from '@app/core/api/decorators/is-greater-than.decorator';
 
 export class SearchJobsQueryDto {
   @LimitParam({ default: 10 })
@@ -7,4 +11,23 @@ export class SearchJobsQueryDto {
 
   @PageParam({ default: 0 })
   readonly page: number = 0;
+
+  @ApiPropertyOptional()
+  @IsString()
+  searchText?: string;
+
+  @ApiPropertyOptional({ enum: EmploymentType })
+  @IsEnum(EmploymentType)
+  employmentType?: EmploymentType;
+
+  @ApiPropertyOptional()
+  @IsPositive()
+  @IsNumber({ allowNaN: false, allowInfinity: false })
+  salaryFrom?: number;
+
+  @ApiPropertyOptional()
+  @IsPositive()
+  @IsNumber({ allowNaN: false, allowInfinity: false })
+  @IsGreaterThan('salaryFrom')
+  salaryTo?: number;
 }
