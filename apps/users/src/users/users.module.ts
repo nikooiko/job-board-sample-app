f997057f7cf47a036fe770bebf80d288badc0599
Nikos Oikonomou <nikoikonomou92@gmail.com>
2023-02-25 19:55:57 +0200

    users: enabled forgotten users controller

--- apps/users/src/users/users.module.ts
+++ apps/users/src/users/users.module.ts
@@ -3,9 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersService } from './services/users.service';
 import usersConfig from './config/users.config';
 import { UsersPrismaModule } from '../users-prisma/users-prisma.module';
+import { UsersController } from './controllers/users.controller';
 
 @Module({
   imports: [ConfigModule.forFeature(usersConfig), UsersPrismaModule],
+  controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
 })
