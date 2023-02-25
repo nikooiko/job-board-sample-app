15207a95426cd2763950068e720f93b7b9b07594
Nikos Oikonomou <nikoikonomou92@gmail.com>
2023-02-25 20:25:13 +0200

    users: seeds are now hashing passwords

--- libs/testing/src/users/seeds/user-seeds.ts
+++ libs/testing/src/users/seeds/user-seeds.ts
@@ -1,4 +1,5 @@
 import { Prisma, PrismaClient } from '@app/users/prisma-client';
+import bcrypt from 'bcrypt';
 
 // Can you generate me uuid with 0?
 //
@@ -8,17 +9,17 @@ export const userSeeds = async (prisma: PrismaClient) => {
     {
       id: '00000000-0000-0000-0000-000000000001',
       email: 'user1@jb-sample.app',
-      password: 'user1pass',
+      password: await bcrypt.hash('user1pass', 10),
     },
     {
       id: '00000000-0000-0000-0000-000000000002',
       email: 'user2@jb-sample.app',
-      password: 'user2pass',
+      password: await bcrypt.hash('user2pass', 10),
     },
     {
       id: '00000000-0000-0000-0000-000000000003',
       email: 'user3@jb-sample.app',
-      password: 'user3pass',
+      password: await bcrypt.hash('user2pass', 10),
     },
   ];
   const profiles: Prisma.ProfileCreateManyInput[] = [
